4382c9951e74ab270649ad00ac3c150e92c5193e
DoVuDung <vudungoik2016@gmail.com>
2021-08-06 18:52:25 +0700

    còn lỗi mssql

--- mssql_test.js
+++ mssql_test.js
@@ -1,27 +1,52 @@
-const sql = require('mssql')
-const sqlConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    server: 'localhost',
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
+var sql = require('mssql');
+var sqlConfig = {
+    server: 'ANDY\\LOCALDB',
+    user: 'sa',
+    password: 'a123456*',
+    database: 'test',
+    port: 1433,
     options: {
-        encrypt: true, // for azure
-        trustServerCertificate: false // change to true for local dev / self-signed certs
+        encrypt: false
     }
-}
+};
 
-(async() => {
-    try {
-        // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect(sqlConfig)
-        const result = await sql.query `select * from Personal`
-        console.log(result)
-    } catch (err) {
-        // ... error checks
-    }
-})()
+function getList() {
+    var conn = new sql.ConnectionPool(sqlConfig);
+
+    conn.connect(function(err) {
+        //if (err) throw err;
+        var req = new sql.Request(conn);
+        req.query('select * from Persons', function(err, recordset) {
+            if (err) throw err;
+            else
+                console.log(recordset);
+            conn.close();
+        });
+
+    });
+};
+getList();
+
+
+// sql.connect(sqlConfig, function(err) {
+//     if (err) { console.log(err); }
+//     let sqlRequest = new sql.Request();
+//     let sqlQuery = `select * from Persons`;
+//     sqlRequest.query(sqlQuery, function(err, data) {
+//         if (err) { console.log(err); }
+//         console.table(data.recordset);
+//     });
+//     sql.close();
+// });
+
+
+// (async() => {
+//     try {
+//         // make sure that any items are correctly URL encoded in the connection string
+//         await sql.connect(sqlConfig)
+//         const result = await sql.query `select * from Personal`
+//         console.log(result)
+//     } catch (err) {
+//         // ... error checks
+//     }
+// })();
